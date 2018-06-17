cd9d44d3446759e624f928010b867c3523ab0507
u2berggeist <jrwrigh.iii@gmail.com>
2018-06-16 20:23:38 -0400

    added public testing csv

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ from decimal import Decimal
 
 write_to_book = False
 
-path_to_CSV = Path.cwd() / 'transactions_test.csv'
+path_to_CSV = Path.cwd() / 'transactions_testPUBLIC.csv'
+# path_to_CSV = Path.cwd() / 'transactions_test.csv'
 # path_to_CSV = Path.cwd() / 'transactions.csv'
 path_to_Book = Path.cwd() / 'test.gnucash'
 path_to_translationJSON = Path.cwd() / 'translations.json'
