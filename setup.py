45780eca7731b1fa104fb96ff94ee4c8cec20eed
u2berggeist <jrwrigh.iii@gmail.com>
2018-07-02 11:50:58 -0400

    Updated versioning number in `setup.py`

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="csv2cash",
-    version="0.1.0",
+    version="0.2.4",
     author="u2berggeist",
     description="Module to process CSV and import into GNUCash",
     long_description=long_description,
