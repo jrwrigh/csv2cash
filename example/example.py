64d8922da59121efddec00b551092828ac3e904b
u2berggeist <jrwrigh.iii@gmail.com>
2018-08-09 10:59:59 -0400

    added dfgui section to example.py

--- example/example.py
+++ example/example.py
@@ -1,8 +1,14 @@
 import csv2cash
 from pathlib import Path
+# import dfgui # <- for DF visualization
 
 path_to_CSV = Path.cwd() / 'transactions_testPUBLIC.csv'
 path_to_Book = Path.cwd() / 'test.gnucash'
 path_to_translationJSON = Path.cwd() / 'translations.json'
+
+# # For checking the translations
+# transfer_uncat = csv2cash.get_uncat_transfers(path_to_CSV,
+#                                               path_to_translationJSON)
+# dfgui.show(transfer_uncat)
 
 csv2cash.do_csv2cash(path_to_Book, path_to_CSV, path_to_translationJSON)
