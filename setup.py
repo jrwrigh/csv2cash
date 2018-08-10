31574d25db3326c72731d2a457388083a5c71a7a
u2berggeist <jrwrigh.iii@gmail.com>
2018-08-10 11:23:56 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="csv2cash",
-    version="0.3.1",
+    version="0.3.2",
     author="u2berggeist",
     description="Module to process CSV and import into GNUCash",
     long_description=long_description,
